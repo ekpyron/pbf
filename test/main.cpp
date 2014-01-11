9855133b4c3270fffa67ce914d49b5868d5b93e8
Daniel Kirchner <daniel@ekpyron.org>
2014-01-11 14:15:11 +0100

    More debug output for test application.

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <glcorew.h>
+#include <vector>
 
 int main (int argc, char *argv[])
 {
@@ -24,8 +25,13 @@ int main (int argc, char *argv[])
     // get OpenGL entry points
     glcorewInit ((glcorewGetProcAddressCallback) glfwGetProcAddress);
 
+    std::cout << "Window/OpenGL initialization complete." << std::endl;
+
+    std::cout << "Create program/shader objects:" << std::endl;
     GLint program = glCreateProgram ();
     GLint shader = glCreateShader (GL_COMPUTE_SHADER);
+    std::cout << "DONE." << std::endl;
+
     const GLchar *src = "#version 430 core\n"
             "layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;\n"
             "layout (std430, binding = 0) buffer Buffer { float data[]; };\n"
@@ -33,10 +39,35 @@ int main (int argc, char *argv[])
             "{\n"
             " data[0] = 42;\n"
             "}\n";
+
+    std::cout << "Compile shader source:" << std::endl;
     glShaderSource (shader, 1, &src, NULL);
     glCompileShader (shader);
+
+    // check the compilation status and throw the error log as exception on failure
+    GLint status;
+    glGetShaderiv (shader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        std::vector<char> data;
+        GLint length;
+        glGetShaderiv (shader, GL_INFO_LOG_LENGTH, &length);
+        data.resize (length);
+        glGetShaderInfoLog (shader, length, NULL, &data[0]);
+        glDeleteShader (shader);
+        throw std::runtime_error (std::string ("Cannot compile shader:")
+                + std::string (&data[0], length));
+    }
+
+    std::cout << "DONE." << std::endl;
+
+    std::cout << "Link shader program:" << std::endl;
+
     glAttachShader (program, shader);
     glLinkProgram (program);
+    std::cout << "DONE." << std::endl;
+
+    std::cout << "Prepare execution:" << std::endl;
     glUseProgram (program);
 
     GLuint buffer;
@@ -48,7 +79,9 @@ int main (int argc, char *argv[])
 
     glGetBufferSubData (GL_SHADER_STORAGE_BUFFER, 0, sizeof (float), &data);
 
-    std::cout << "Before: " << data << std::endl;
+    std::cout << "DONE." << std::endl;
+
+    std::cout << "Before execution: " << data << std::endl;
 
     glDispatchCompute (1, 1, 1);
 
@@ -57,10 +90,14 @@ int main (int argc, char *argv[])
 
     glGetBufferSubData (GL_SHADER_STORAGE_BUFFER, 0, sizeof (float), &data);
 
-    std::cout << "After: " << data << std::endl;
+    std::cout << "After execution: " << data << std::endl;
+
+    std::cout << "TEST FINISHED" << std::endl;
 
     glfwDestroyWindow (window);
     glfwTerminate ();
+
+    std::cout << "EXITING" << std::endl;
 
     return 0;
 
