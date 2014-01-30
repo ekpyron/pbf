9ef8aa006f426150b7076d2ef3ecd426165e36f0
Daniel Kirchner <daniel@ekpyron.org>
2014-01-30 01:43:50 +0100

    Conflict free offsets for radix blockscan.

--- shaders/radixsort/blockscan.glsl
+++ shaders/radixsort/blockscan.glsl
@@ -15,7 +15,11 @@ layout (std430, binding = 1) writeonly buffer BlockSums
 	uint blocksums[];
 };
 
-shared uint temp[BLOCKSIZE];
+#define NUM_BANKS 16
+#define LOG_NUM_BANKS 4
+#define CONFLICT_FREE_OFFSET(n) ((n) >> LOG_NUM_BANKS)
+
+shared uint temp[BLOCKSIZE + CONFLICT_FREE_OFFSET(BLOCKSIZE)];
 
 const int n = BLOCKSIZE;
 
@@ -25,8 +29,14 @@ void main (void)
 	int lid = int (gl_LocalInvocationIndex);
 	int offset = 1;
 	
-	temp[2 * lid] = data[2 * gid];
-	temp[2 * lid + 1] = data[2 * gid + 1];
+	int ai = lid;
+	int bi = lid + (n/2);
+	
+	int bankOffsetA = CONFLICT_FREE_OFFSET(ai);
+	int bankOffsetB = CONFLICT_FREE_OFFSET(bi);
+	
+	temp[ai + bankOffsetA] = data[gid];
+	temp[bi + bankOffsetB] = data[gid + (n/2)];
 	
 	for (int d = n >> 1; d > 0; d >>= 1)
 	{
@@ -37,6 +47,8 @@ void main (void)
 		{
 			int ai = offset * (2 * lid + 1) - 1;
 			int bi = offset * (2 * lid + 2) - 1;
+			ai += CONFLICT_FREE_OFFSET(ai);
+			bi += CONFLICT_FREE_OFFSET(bi);
 			
 			temp[bi] += temp[ai]; 
 		}
@@ -45,8 +57,8 @@ void main (void)
 	
 	if (lid == 0)
 	{
-		blocksums[gl_WorkGroupID.x] = temp[n - 1];
-		temp[n - 1] = 0;
+		blocksums[gl_WorkGroupID.x] = temp[n - 1 + CONFLICT_FREE_OFFSET(n-1)];
+		temp[n - 1 + CONFLICT_FREE_OFFSET(n-1)] = 0;
 	}
 	
 	for (int d = 1; d < n; d *= 2)
@@ -59,6 +71,8 @@ void main (void)
 		{
 			int ai = offset * (2 * lid + 1) - 1;
 			int bi = offset * (2 * lid + 2) - 1;
+			ai += CONFLICT_FREE_OFFSET(ai);
+			bi += CONFLICT_FREE_OFFSET(bi);
 			
 			uint t = temp[ai];
 			temp[ai] = temp[bi];
@@ -69,6 +83,6 @@ void main (void)
 	barrier ();
 	memoryBarrierShared ();
 	
-	data[2 * gid] = temp[2 * lid];
-	data[2 * gid + 1] = temp[2 * lid + 1];
+	data[gid] = temp[ai + bankOffsetA];
+	data[gid + (n/2)] = temp[bi + bankOffsetB];
 }
