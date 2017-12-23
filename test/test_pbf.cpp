b2bd02f64def73dfa35c290ce4f602395f388610
Moritz Hoffmann <clonker@gmail.com>
2017-12-24 00:26:45 +0100

    split up pbf into library and main, added catch2 as testing FW

---
+++ test/test_pbf.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file test_pbf.cpp
+ * @author clonker
+ * @date 12/24/17
+ */
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
