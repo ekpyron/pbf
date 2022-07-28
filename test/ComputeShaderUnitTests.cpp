efd5d6ba25373b60f631b49f4f2ec0e917bdc656
Daniel Kirchner <daniel@ekpyron.org>
2022-07-28 22:27:48 +0200

    Cleanup.

--- test/ComputeShaderUnitTests.cpp
+++ test/ComputeShaderUnitTests.cpp
@@ -9,9 +9,11 @@
 #endif
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_templated.hpp>
 #include "ComputeShaderUnitTestContext.h"
 #include <ranges>
 #include <random>
+#include <span>
 
 using ComputeShaderUnitTest = pbf::test::ComputeShaderUnitTestContext;
 
@@ -148,6 +150,13 @@ TEST_CASE_METHOD(ComputeShaderUnitTest, "Bitfield Extract", "[bitfield extract]"
 	CHECK(bitfieldExtract(0b1100, 2, 2) == 0b11);
 }
 
+auto roundToNextMultiple(auto value, auto mod) {
+	if (value % mod) {
+		return value + (mod - (value % mod));
+	} else {
+		return value;
+	}
+}
 
 TEST_CASE("CPU Sort Test", "[cpu sort]")
 {
@@ -159,28 +168,13 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 		for (size_t i = 0; i < 32 * 32 * 33 + 16; ++i)
 			initialKeys.emplace_back(distrib(gen));
 	}
-	const uint32_t numKeys = static_cast<uint32_t>(initialKeys.size());
+	const auto numKeys = static_cast<uint32_t>(initialKeys.size());
 	const uint32_t sortBitCount = 32;
 	const uint32_t blockSize = 16;
 	const uint32_t BLOCKSIZE = blockSize;
 	const uint32_t halfBlockSize = blockSize / 2;
 	const uint32_t HALFBLOCKSIZE = halfBlockSize;
 	const uint32_t numGroups = (numKeys + blockSize - 1) / blockSize;
-
-	const uint32_t LOG2HALFBLOCKSIZE = []()
-	{
-		uint result = 0;
-		for (uint d = blockSize / 2; d > 0; d /= 2)
-			result++;
-		return result;
-	}();
-	const uint32_t LOG2BLOCKSIZE = []()
-	{
-		uint result = 0;
-		for (uint d = 1; d < blockSize; d *= 2)
-			result++;
-		return result;
-	}();
 
 	auto keys = initialKeys;
 	auto GetHash = [&keys](uint32_t index) {
@@ -196,7 +190,7 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 
 	std::vector<std::vector<uint32_t>> blockSums;
 	for (ssize_t blockSumSize = 4 * numGroups; blockSumSize > 1; blockSumSize = (blockSumSize + blockSize - 1) / blockSize) {
-		blockSums.emplace_back((blockSumSize + blockSize - 1)&~(blockSize - 1));
+		blockSums.emplace_back(roundToNextMultiple(blockSumSize, blockSize));
 	}
 	blockSums.emplace_back(blockSize);
 
@@ -232,8 +226,7 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 					uvec4(equal(bits2 * uvec4(1, 1, 1, 1), uvec4(0, 1, 2, 3)));
 			}
 			int offset = 1;
-			uint d = HALFBLOCKSIZE;
-			for (uint c = 0; c < LOG2HALFBLOCKSIZE; c++)
+			for (uint d = blockSize / 2; d > 0; d /= 2)
 			{
 				for (uint32_t gl_LocalInvocationIndex = 0; gl_LocalInvocationIndex < halfBlockSize; gl_LocalInvocationIndex++)
 				{
@@ -247,7 +240,6 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 					}
 				}
 				offset *= 2;
-				d /= 2;
 			}
 			for (uint32_t gl_LocalInvocationIndex = 0; gl_LocalInvocationIndex < halfBlockSize; gl_LocalInvocationIndex++)
 			{
@@ -261,8 +253,7 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 					tmp[BLOCKSIZE - 1] = uvec4(0, 0, 0, 0);
 				}
 			}
-			d = 1;
-			for (uint ctr = 0; ctr < LOG2BLOCKSIZE; ctr++)
+			for (uint d = 1; d < blockSize; d *= 2)
 			{
 				offset /= 2;
 
@@ -277,7 +268,6 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 						tmp[j] += t;
 					}
 				}
-				d *= 2;
 			}
 
 			for (uint32_t gl_LocalInvocationIndex = 0; gl_LocalInvocationIndex < halfBlockSize; gl_LocalInvocationIndex++)
@@ -298,7 +288,7 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 		}
 
 		// scan
-		auto scan = [BLOCKSIZE, HALFBLOCKSIZE, LOG2BLOCKSIZE, LOG2HALFBLOCKSIZE](auto& prefixSum, auto& blockSum) {
+		auto scan = [BLOCKSIZE, HALFBLOCKSIZE](auto& prefixSum, auto& blockSum) {
 			// const uint32_t numBlockSums_alt = (4 * numGroups + blockSize - 1) / blockSize;
 			const uint32_t numBlockSums = (prefixSum.size() + blockSize - 1) / blockSize;
 			for (uint32_t workGroupID = 0; workGroupID < numBlockSums; workGroupID++)
@@ -314,8 +304,7 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 						prefixSum[gl_WorkGroupID.x * BLOCKSIZE + gl_LocalInvocationIndex + HALFBLOCKSIZE];
 				}
 				int offset = 1;
-				uint d = HALFBLOCKSIZE;
-				for (uint ctr = 0; ctr < LOG2HALFBLOCKSIZE; ctr++)
+				for (uint d = blockSize / 2; d > 0; d /= 2)
 				{
 					for (uint32_t gl_LocalInvocationIndex = 0; gl_LocalInvocationIndex < halfBlockSize; gl_LocalInvocationIndex++)
 					{
@@ -331,7 +320,6 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 						}
 					}
 					offset *= 2;
-					d /= 2;
 				}
 				for (uint32_t gl_LocalInvocationIndex = 0; gl_LocalInvocationIndex < halfBlockSize; gl_LocalInvocationIndex++)
 				{
@@ -342,24 +330,18 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 					}
 				}
 
-				d = 1;
+				for (uint d = 1; d < blockSize; d *= 2)
 				{
+					offset /= 2;
+					for (uint32_t gl_LocalInvocationIndex = 0; gl_LocalInvocationIndex < halfBlockSize; gl_LocalInvocationIndex++)
 					{
-						for (uint ctr = 0; ctr < LOG2BLOCKSIZE; ctr++)
+						if (gl_LocalInvocationIndex < d)
 						{
-							offset /= 2;
-							for (uint32_t gl_LocalInvocationIndex = 0; gl_LocalInvocationIndex < halfBlockSize; gl_LocalInvocationIndex++)
-							{
-								if (gl_LocalInvocationIndex < d)
-								{
-									uint i = offset * (2 * gl_LocalInvocationIndex + 1) - 1;
-									uint j = offset * (2 * gl_LocalInvocationIndex + 2) - 1;
-									uint t = tmp[i];
-									tmp[i] = tmp[j];
-									tmp[j] += t;
-								}
-							}
-							d *= 2;
+							uint i = offset * (2 * gl_LocalInvocationIndex + 1) - 1;
+							uint j = offset * (2 * gl_LocalInvocationIndex + 2) - 1;
+							uint t = tmp[i];
+							tmp[i] = tmp[j];
+							tmp[j] += t;
 						}
 					}
 				}
@@ -393,12 +375,6 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 			addblocksums(blockSums[i - 1], blockSums[i]);
 		}
 
-/*		std::vector<uint32_t> blockSum2((blockSum.size() + blockSize - 1) / blockSize);
-
-		scan(blockSum, blockSum2);*/
-
-		// TODO: propagate block sums back down, i.e. addblocksum
-
 		// globalsort
 		for (uint32_t workGroupID = 0; workGroupID < numGroups; workGroupID++)
 		{
@@ -422,40 +398,61 @@ TEST_CASE("CPU Sort Test", "[cpu sort]")
 	}
 }
 
+template<typename Range>
+struct EqualsRangeMatcher : Catch::Matchers::MatcherGenericBase {
+	EqualsRangeMatcher(Range const& range):
+		range{ range }
+	{}
+
+	template<typename OtherRange>
+	bool match(OtherRange const& other) const {
+		using std::begin; using std::end;
+
+		return std::equal(begin(range), end(range), begin(other), end(other));
+	}
+
+	std::string describe() const override {
+		return "Equals: " + Catch::rangeToString(range);
+	}
+
+private:
+	Range const& range;
+};
+
+template<typename Range>
+auto EqualsRange(const Range& range) -> EqualsRangeMatcher<Range> {
+	return EqualsRangeMatcher<Range>{range};
+}
+
 TEST_CASE_METHOD(ComputeShaderUnitTest, "Compute Shader Sort Test", "[sort]")
 {
 	using namespace pbf;
 
+	const size_t maxParticleCount = 128*128*128;
+	const uint32_t sortBitCount = 32; // sort up to # of bits
+	const uint32_t blockSize = 256;
 
 	std::vector<uint32_t> initialKeys;
 
 	{
-		std::random_device rd;  //Will be used to obtain a seed for the random number engine
-		std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+		std::random_device rd;
+		std::mt19937 gen(rd());
 		std::uniform_int_distribution<uint32_t> distrib(0, std::numeric_limits<uint32_t>::max());
-		for (size_t i = 0; i < 32 * 32 * 32 + 32 + 32 + 76; ++i)
+		for (size_t i = 0; i < std::uniform_int_distribution<size_t>(0, maxParticleCount)(gen); ++i)
 			initialKeys.emplace_back(distrib(gen));
-
 	}
 
-	/*{
-		auto rd = std::random_device {};
-		auto rng = std::default_random_engine { rd() };
-		std::shuffle(std::begin(initialKeys), std::end(initialKeys), rng);
-	}*/
-	/*for (auto v: initialKeys)
-		UNSCOPED_INFO("Key: " + std::to_string(v));*/
 
-	const uint32_t blockSize = 32; // apparently has to be >= initialKeys.size() / 2 right now
-
-	while (initialKeys.size() % blockSize) {
-		initialKeys.emplace_back(std::numeric_limits<uint32_t>::max());
+	if (initialKeys.size() % blockSize)
+	{
+		initialKeys.reserve(roundToNextMultiple(initialKeys.size(), blockSize));
+		while (initialKeys.size() % blockSize)
+			initialKeys.emplace_back(std::numeric_limits<uint32_t>::max());
 	}
-	
-	const uint32_t numKeys = static_cast<uint32_t>(initialKeys.size());
+
+	const auto numKeys = static_cast<uint32_t>(initialKeys.size());
 
 
-	const uint32_t sortBitCount = 32; // sort up to # of bits
 	struct PushConstants {
 		glm::u32vec4 blockSumOffset;
 		uint32_t bit = 0;
@@ -502,7 +499,7 @@ TEST_CASE_METHOD(ComputeShaderUnitTest, "Compute Shader Sort Test", "[sort]")
 	for (ssize_t blockSumSize = 4 * numGroups; blockSumSize > 1; blockSumSize = (blockSumSize + blockSize - 1) / blockSize) {
 		blockSums.emplace_back(
 			this,
-			((blockSumSize + blockSize - 1) & ~(blockSize - 1)) * sizeof(uint32_t),
+			roundToNextMultiple(blockSumSize, blockSize) * sizeof(uint32_t),
 			vk::BufferUsageFlagBits::eStorageBuffer,
 			MemoryType::STATIC
 		);
@@ -562,32 +559,27 @@ TEST_CASE_METHOD(ComputeShaderUnitTest, "Compute Shader Sort Test", "[sort]")
 
 layout (local_size_x_id = 0, local_size_y = 1, local_size_z = 1) in;
 
-precision highp float;
-precision highp int;
-
 const uint HALFBLOCKSIZE = gl_WorkGroupSize.x;
 const uint BLOCKSIZE = 2 * HALFBLOCKSIZE;
-layout (constant_id = 1) const uint LOG2HALFBLOCKSIZE = 0;
-layout (constant_id = 2) const uint LOG2BLOCKSIZE = 0;
 
-layout(std430, binding = 0) readonly volatile coherent  buffer Keys
+layout(std430, binding = 0) readonly buffer Keys
 {
     uint keys[];
-};
-
-layout(std430, binding = 1) writeonly volatile coherent  buffer PrefixSums
-{
-    uint prefixSum[];
-};
-
-layout(std430, binding = 2) writeonly volatile coherent  buffer BlockSums
-{
-    uint blockSum[];
 };
 
 uint GetHash(uint id) {
 	return keys[id];
 }
+
+layout(std430, binding = 1) writeonly buffer PrefixSums
+{
+    uint prefixSum[];
+};
+
+layout(std430, binding = 2) writeonly buffer BlockSums
+{
+    uint blockSum[];
+};
 
 layout(push_constant) uniform constants {
 	uvec4 blockSumOffsets;
@@ -595,12 +587,6 @@ layout(push_constant) uniform constants {
 };
 
 shared uvec4 tmp[BLOCKSIZE];
-
-// TODO: what *exactly* is needed :-)?
-//#define BARRIER() barrier(); memoryBarrier(); memoryBarrierShared(); groupMemoryBarrier(); barrier()
-//#define BARRIER() memoryBarrier(); groupMemoryBarrier(); barrier()
-#define BARRIER() memoryBarrier(); barrier()
-//#define BARRIER() barrier()
 
 void main()
 {
@@ -612,73 +598,53 @@ void main()
 
 
     int offset = 1;
+	for (uint d = HALFBLOCKSIZE; d > 0; d /= 2)
 	{
-		uint d = HALFBLOCKSIZE;
-		for (uint c = 0; c < LOG2HALFBLOCKSIZE; c++)
-		{
-			BARRIER();
+		barrier();
 
-			if (gl_LocalInvocationIndex < d) {
-				uint i = offset * (2 * gl_LocalInvocationIndex + 1) - 1;
-				uint j = offset * (2 * gl_LocalInvocationIndex + 2) - 1;
-				tmp[j] += tmp[i];
-			}
-			offset *= 2;
-			d /= 2;
+		if (gl_LocalInvocationIndex < d) {
+			uint i = offset * (2 * gl_LocalInvocationIndex + 1) - 1;
+			uint j = offset * (2 * gl_LocalInvocationIndex + 2) - 1;
+			tmp[j] += tmp[i];
 		}
+		offset *= 2;
 	}
 
-	BARRIER();
+	barrier();
 
 	if (gl_LocalInvocationIndex == 0)
 	{
 		for(uint i = 0; i < 4; ++i)
-			blockSum[uvec4(blockSumOffsets)[i] + gl_WorkGroupID.x] = uvec4(tmp[BLOCKSIZE - 1])[i];
+			blockSum[blockSumOffsets[i] + gl_WorkGroupID.x] = tmp[BLOCKSIZE - 1][i];
 		tmp[BLOCKSIZE - 1] = uvec4(0, 0, 0, 0);
 	}
 
+	for (uint d = 1; d < BLOCKSIZE; d *= 2)
 	{
-		uint d = 1;
-		for (uint ctr = 0; ctr < LOG2BLOCKSIZE; ctr++)
-		{
-			offset /= 2;
-			BARRIER();
+		offset /= 2;
+		barrier();
 
-			if (gl_LocalInvocationIndex < d)
-			{
-				uint i = offset * (2 * gl_LocalInvocationIndex + 1) - 1;
-				uint j = offset * (2 * gl_LocalInvocationIndex + 2) - 1;
-				uvec4 t = tmp[i];
-				tmp[i] = tmp[j];
-				tmp[j] += t;
-			}
-			d *= 2;
+		if (gl_LocalInvocationIndex < d)
+		{
+			uint i = offset * (2 * gl_LocalInvocationIndex + 1) - 1;
+			uint j = offset * (2 * gl_LocalInvocationIndex + 2) - 1;
+			uvec4 t = tmp[i];
+			tmp[i] = tmp[j];
+			tmp[j] += t;
 		}
 	}
 
-    BARRIER();
+    barrier();
 
-    prefixSum[gl_WorkGroupID.x * BLOCKSIZE + gl_LocalInvocationIndex] = uvec4(tmp[gl_LocalInvocationIndex])[bits1];
-    prefixSum[gl_WorkGroupID.x * BLOCKSIZE + gl_LocalInvocationIndex + HALFBLOCKSIZE] = uvec4(tmp[gl_LocalInvocationIndex + HALFBLOCKSIZE])[bits2];
+    prefixSum[gl_WorkGroupID.x * BLOCKSIZE + gl_LocalInvocationIndex] = tmp[gl_LocalInvocationIndex][bits1];
+    prefixSum[gl_WorkGroupID.x * BLOCKSIZE + gl_LocalInvocationIndex + HALFBLOCKSIZE] = tmp[gl_LocalInvocationIndex + HALFBLOCKSIZE][bits2];
 }
 						)"),
 						PBF_DESC_DEBUG_NAME("Prescan Compute Shader")
 					}),
 				.entryPoint = "main",
 				.specialization = {
-					Specialization<uint32_t>{.constantID = 0, .value = blockSize / 2 },
-					Specialization<uint32_t>{.constantID = 1, .value = [&]() {
-						uint result = 0;
-						for (uint d = blockSize / 2; d > 0; d /= 2)
-							result++;
-						return result;
-					}()},
-					Specialization<uint32_t>{.constantID = 2, .value = [&]() {
-						uint result = 0;
-						for (uint d = 1; d < blockSize; d *= 2)
-							result++;
-						return result;
-					}()}
+					Specialization<uint32_t>{.constantID = 0, .value = blockSize / 2 }
 				}
 			},
 			.pipelineLayout = sortPipelineLayout,
@@ -699,26 +665,15 @@ void main()
 
 layout (local_size_x_id = 0, local_size_y = 1, local_size_z = 1) in;
 
-precision highp float;
-precision highp int;
-
 const uint HALFBLOCKSIZE = gl_WorkGroupSize.x;
 const uint BLOCKSIZE = 2 * HALFBLOCKSIZE;
-layout (constant_id = 1) const uint LOG2HALFBLOCKSIZE = 0;
-layout (constant_id = 2) const uint LOG2BLOCKSIZE = 0;
 
-// TODO: what *exactly* is needed :-)?
-//#define BARRIER() barrier(); memoryBarrier(); memoryBarrierShared(); groupMemoryBarrier(); barrier()
-//#define BARRIER() memoryBarrier(); groupMemoryBarrier(); barrier()
-#define BARRIER() memoryBarrier(); barrier()
-//#define BARRIER() barrier()
-
-layout(std430, binding = 1) volatile coherent buffer PrefixSums
+layout(std430, binding = 1) buffer PrefixSums
 {
     uint prefixSum[];
 };
 
-layout(std430, binding = 2) volatile coherent buffer BlockSums
+layout(std430, binding = 2) buffer BlockSums
 {
     uint blockSum[];
 };
@@ -732,40 +687,32 @@ void main()
 
 
     int offset = 1;
+	for (uint d = HALFBLOCKSIZE; d > 0; d /= 2)
 	{
-		uint d = HALFBLOCKSIZE;
-#pragma unroll_loop_start
-		for (uint ctr = 0; ctr < LOG2HALFBLOCKSIZE; ctr++)
-		{
-			BARRIER();
+		barrier();
 
-			if (gl_LocalInvocationIndex < d) {
-				uint i = offset * (2 * gl_LocalInvocationIndex + 1) - 1;
-				uint j = offset * (2 * gl_LocalInvocationIndex + 2) - 1;
-				tmp[j] += tmp[i];
-			}
-			offset *= 2;
-			d /= 2;
+		if (gl_LocalInvocationIndex < d) {
+			uint i = offset * (2 * gl_LocalInvocationIndex + 1) - 1;
+			uint j = offset * (2 * gl_LocalInvocationIndex + 2) - 1;
+			tmp[j] += tmp[i];
 		}
-#pragma unroll_loop_end
+		offset *= 2;
 	}
 
-    BARRIER();
+    barrier();
 
 	if (gl_LocalInvocationIndex == 0)
 	{
 		blockSum[gl_WorkGroupID.x] = tmp[BLOCKSIZE - 1];
 		tmp[BLOCKSIZE - 1] = 0;
 	}
-    BARRIER();
+    barrier();
 
 	{
-		uint d = 1;
-#pragma unroll_loop_start
-		for (uint ctr = 0; ctr < LOG2BLOCKSIZE; ctr++)
+		for(uint d = 1; d < BLOCKSIZE; d *= 2)
 		{
 			offset /= 2;
-			BARRIER();
+			barrier();
 
 			if (gl_LocalInvocationIndex < d)
 			{
@@ -775,12 +722,10 @@ void main()
 				tmp[i] = tmp[j];
 				tmp[j] += t;
 			}
-			d *= 2;
 		}
-#pragma unroll_loop_end
 	}
 
-    BARRIER();
+    barrier();
 
     prefixSum[gl_WorkGroupID.x * BLOCKSIZE + gl_LocalInvocationIndex] = tmp[gl_LocalInvocationIndex];
     prefixSum[gl_WorkGroupID.x * BLOCKSIZE + gl_LocalInvocationIndex + HALFBLOCKSIZE] = tmp[gl_LocalInvocationIndex + HALFBLOCKSIZE];
@@ -790,19 +735,7 @@ void main()
 					}),
 				.entryPoint = "main",
 				.specialization = {
-					Specialization<uint32_t>{.constantID = 0, .value = blockSize / 2 },
-					Specialization<uint32_t>{.constantID = 1, .value = [&]() {
-						uint result = 0;
-						for (uint d = blockSize / 2; d > 0; d /= 2)
-							result++;
-						return result;
-					}()},
-					Specialization<uint32_t>{.constantID = 2, .value = [&]() {
-						uint result = 0;
-						for (uint d = 1; d < blockSize; d *= 2)
-							result++;
-						return result;
-					}()}
+					Specialization<uint32_t>{.constantID = 0, .value = blockSize / 2 }
 				}
 			},
 			.pipelineLayout = sortPipelineLayout,
@@ -864,29 +797,26 @@ void main()
 #extension GL_ARB_separate_shader_objects : enable
 #extension GL_KHR_shader_subgroup_basic : enable
 
-precision highp float;
-precision highp int;
-
 layout (local_size_x_id = 0, local_size_y = 1, local_size_z = 1) in;
 
 const uint BLOCKSIZE = gl_WorkGroupSize.x;
 
-layout(std430, binding = 0) volatile coherent readonly buffer Keys
+layout(std430, binding = 0) readonly buffer Keys
 {
     uint keys[];
 };
 
-layout(std430, binding = 1) volatile coherent readonly buffer PrefixSums
+layout(std430, binding = 1) readonly buffer PrefixSums
 {
     uint prefixSum[];
 };
 
-layout(std430, binding = 2) volatile coherent readonly buffer BlockSums
+layout(std430, binding = 2) readonly buffer BlockSums
 {
     uint blockSum[];
 };
 
-layout(std430, binding = 3) volatile coherent writeonly buffer Result
+layout(std430, binding = 3) writeonly buffer Result
 {
     uint result[];
 };
@@ -900,16 +830,14 @@ layout(push_constant) uniform constants {
     int bitShift;
 };
 
+uint GetPermutation(uint id) {
+	uint bits = bitfieldExtract(GetHash(id), bitShift, 2);
+	return blockSum[blockSumOffsets[bits] + gl_WorkGroupID.x] + prefixSum[gl_GlobalInvocationID.x];
+}
+
 void main()
 {
-	uint bits = bitfieldExtract(GetHash(gl_GlobalInvocationID.x), bitShift, 2);
-
-	//result[gl_GlobalInvocationID.x] = blockSum[blockSumOffsets[bits] + gl_WorkGroupID.x] + prefixSum[gl_GlobalInvocationID.x];
-
-	result[blockSum[blockSumOffsets[bits] + gl_WorkGroupID.x] + prefixSum[gl_GlobalInvocationID.x]] = keys[gl_GlobalInvocationID.x];
-
-
-//result[gl_GlobalInvocationID.x] = blockSum[blockSumOffsets[bits] + gl_WorkGroupID.x] + prefixSum[gl_GlobalInvocationID.x];
+	result[GetPermutation(gl_GlobalInvocationID.x)] = keys[gl_GlobalInvocationID.x];
 }
 						)"),
 						PBF_DESC_DEBUG_NAME("Global Sort Compute Shader")
@@ -1005,8 +933,6 @@ void main()
 		}}, {});
 	}
 
-	uint32_t barrierQueueFamily = VK_QUEUE_FAMILY_IGNORED;
-
 	run([&](vk::CommandBuffer buf) {
 
 		buf.pipelineBarrier(vk::PipelineStageFlagBits::eHost, vk::PipelineStageFlagBits::eTransfer, {}, {
@@ -1071,7 +997,6 @@ void main()
 			}, {}, {});
 		}
 
-		// TODO: addblocksums
 		for (ssize_t i = blockSums.size() - 2; i > 0; i--) {
 			auto& prefixSum = blockSums[i - 1];
 			const uint32_t numBlockSums = ((prefixSum.size() / sizeof(uint32_t)) + blockSize - 1) / blockSize;
@@ -1143,25 +1068,6 @@ void main()
 		}, {}, {});
 	});
 
-	device().waitIdle();
-	hostBuffer.flush();
-	device().waitIdle();
-
-	{
-		UNSCOPED_INFO("keys");
-		uint32_t *data = hostBuffer.as<uint32_t>();
-		size_t numKeys = hostBuffer.size() / sizeof(uint32_t);
-		std::sort(initialKeys.begin(), initialKeys.end());
-		bool allFine = true;
-		for (size_t i = 0; i < numKeys; ++i)
-		{
-			/*UNSCOPED_INFO(std::to_string(i) + ": " + std::to_string(data[i]) +
-						  (i < numKeys - 1 ? (data[i] <= data[i+1] ? " <=" : " >") : "") +
-						  "    [" + (data[i] == initialKeys[i] ? "==" : "!=") + " reference (" + std::to_string(initialKeys[i]) + ")]"
-			);*/
-			if (data[i] != initialKeys[i])
-				allFine = false;
-		}
-		CHECK(allFine);
-	}
+	std::ranges::sort(initialKeys);
+	REQUIRE_THAT(std::span(hostBuffer.as<uint32_t>(), hostBuffer.size() / sizeof(uint32_t)), EqualsRange(initialKeys));
 }
