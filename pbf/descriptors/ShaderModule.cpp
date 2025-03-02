8d5f7996c254bc4d31559c6cd65454547d5d9494
Daniel Kirchner <daniel@ekpyron.org>
2025-03-02 18:05:00 +0100

    Fixed sizing of spirv vectors and initial reflection.

--- pbf/descriptors/ShaderModule.cpp
+++ pbf/descriptors/ShaderModule.cpp
@@ -8,6 +8,7 @@
  */
 #include "ShaderModule.h"
 
+#include <spirv_reflect.h>
 #include <pbf/VulkanContext.h>
 #include <fstream>
 
@@ -18,7 +19,7 @@ template<typename... Args> LambdaVisitor(Args...) -> LambdaVisitor<Args...>;
 
 pbf::ShaderModulePtr ShaderModule::realize(ContextInterface &context) const {
     const auto &device = context.device();
-	auto result = std::make_unique<pbf::ShaderModule>();
+	auto shaderModule = std::make_unique<pbf::ShaderModule>();
 
 	std::vector<uint32_t> spirvCodeFromFile;
     std::vector<uint32_t> const& spirvCode = std::visit(LambdaVisitor{
@@ -30,7 +31,7 @@ pbf::ShaderModulePtr ShaderModule::realize(ContextInterface &context) const {
 				f.seekg(0, std::ios_base::end);
 				auto length = static_cast<std::size_t>(f.tellg());
 				f.seekg(0, std::ios_base::beg);
-				spirvCodeFromFile.resize((length + 3) & ~3);
+				spirvCodeFromFile.resize(((length + 3) & ~3) / sizeof(uint32_t));
 				f.read(reinterpret_cast<char *>(spirvCodeFromFile.data()), length);
 				return spirvCodeFromFile;
 			} catch (...) {
@@ -42,10 +43,23 @@ pbf::ShaderModulePtr ShaderModule::realize(ContextInterface &context) const {
 		}
 	}, source);
 
+	SpvReflectShaderModule module = {};
+	SpvReflectResult result = spvReflectCreateShaderModule2(SPV_REFLECT_MODULE_FLAG_NO_COPY, spirvCode.size() * sizeof(uint32_t), spirvCode.data(), &module);
+	assert(result == SPV_REFLECT_RESULT_SUCCESS);
 
-	result->shaderModule = device.createShaderModuleUnique(vk::ShaderModuleCreateInfo({
-		.codeSize = static_cast<uint32_t>(spirvCode.size()),
+	uint32_t count = 0;
+	result = spvReflectEnumerateDescriptorSets(&module, &count, NULL);
+	assert(result == SPV_REFLECT_RESULT_SUCCESS);
+
+	std::vector<SpvReflectDescriptorSet*> sets(count);
+	result = spvReflectEnumerateDescriptorSets(&module, &count, sets.data());
+	assert(result == SPV_REFLECT_RESULT_SUCCESS);
+
+	spvReflectDestroyShaderModule(&module);
+
+	shaderModule->shaderModule = device.createShaderModuleUnique(vk::ShaderModuleCreateInfo({
+		.codeSize = static_cast<uint32_t>(spirvCode.size() * sizeof(uint32_t)),
 		.pCode = spirvCode.data()
 	}));
-	return result;
+	return shaderModule;
 }
